f789bbd09e4e4afaa655ff01aa883a996047c928
MichaelLetson <mletsongamble@gmail.com>
2022-10-27 20:58:19 +0000

    final amendments to settings and Procfile for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn avenger_stuff.wsgi:application
--- avenger_stuff/settings.py
+++ avenger_stuff/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4i%%2+fw6+s1-$)gg)6^jiqkv&74r^+6+1c7(eu@30#ugr-ji8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Project5-avenger-stuff.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,12 +108,17 @@ WSGI_APPLICATION = 'avenger_stuff.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
