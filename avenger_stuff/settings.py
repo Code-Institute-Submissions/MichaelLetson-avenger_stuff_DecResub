21f775da26640093a2a6ea9b5d22ab81a94da276
MichaelLetson <mletsongamble@gmail.com>
2022-10-28 13:11:05 +0000

    edited allowed hosts in settings to fix deployment issue

--- avenger_stuff/settings.py
+++ avenger_stuff/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Project5-avenger-stuff.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['avenger-stuff.herokuapp.com', 'localhost']
 
 
 # Application definition
