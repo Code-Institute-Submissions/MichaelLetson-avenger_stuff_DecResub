4fd2aa0ebac5bda7a33c493990981739e81b2312
MichaelLetson <mletsongamble@gmail.com>
2022-10-31 21:35:31 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Lz59MJpxLe7ySg1XMj9Ah3uHQ6olVkB5FVCs576q7garkXS5QCzZNgDqVoqVidPwSI5YryD4S9mx2ctTaJfTXdQ00tIhRrlGo',  # noqa
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
